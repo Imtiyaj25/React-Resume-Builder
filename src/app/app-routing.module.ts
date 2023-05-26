98fc32e74dd7d41261189d054a168cbc306309f2
Imtiyaj <129411789+Imtiyaj25@users.noreply.github.com>
2023-05-26 23:41:54 +0530

    IS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { FeaturedComponent } from './featured/featured.component';
 
 const routes: Routes = [
   {
     component:HomeComponent,
     path:''
+  },
+  {
+    component:FeaturedComponent,
+    path:'featured'
   }
 ];
 
