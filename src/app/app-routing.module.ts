9f97ecaa450867a74304c58fdee95e0dc4c2694d
Imtiyaj <129411789+Imtiyaj25@users.noreply.github.com>
2023-05-29 22:08:02 +0530

    generating store components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FeaturedComponent } from './featured/featured.component';
+import { StoreComponent } from './store/store.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     component:FeaturedComponent,
     path:'featured'
+  },
+  {
+    component:StoreComponent,
+    path:'store'
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { FeaturedComponent } from './featured/featured.component';
+import { StoreComponent } from './store/store.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FeaturedComponent } from './featured/featured.component';
     NavbarComponent,
     HomeComponent,
     ProductsComponent,
-    FeaturedComponent
+    FeaturedComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
